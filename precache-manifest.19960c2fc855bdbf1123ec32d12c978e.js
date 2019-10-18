72e0dcb8ad520bd313ac9aea5ab99a98c48ac970
b06901135 <b06901135@ntu.edu.tw>
2019-10-18 21:00:20 +0800

    fix routing issue

---
+++ precache-manifest.19960c2fc855bdbf1123ec32d12c978e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "de07f863c8d2b9abd35a",
+    "url": "./static/js/main.61bf51e7.chunk.js"
+  },
+  {
+    "revision": "182ce7a739fa446f855a",
+    "url": "./static/js/2.849e9cb3.chunk.js"
+  },
+  {
+    "revision": "8359419e42b1bb1c05b84cad8fc4706e",
+    "url": "./index.html"
+  }
+];
